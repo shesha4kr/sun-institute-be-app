656fbbbe53fceec35b54f7631ca258bb9f574e7f
shesha4kr <shesha4kr@gmail.com>
2021-08-31 19:27:36 +0530

    created PUT /profile/{studId} endpoint

--- src/main/java/sun/institute/controller/StudentController.java
+++ src/main/java/sun/institute/controller/StudentController.java
@@ -8,12 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
 import sun.institute.data.LoadDbData;
 import sun.institute.data.LoginSuccessDTO;
+import sun.institute.data.StudAuthDetails;
 import sun.institute.data.StudentProfile;
 import sun.institute.model.AITRecord;
 import sun.institute.model.StudentDetails;
@@ -39,27 +45,44 @@ public class StudentController {
 		loadDb.setDbData();
 		return (List<StudentDetails>) studentRepo.findAll();
 	}
-	
+
 	@GetMapping("/profile/{studId}")
 	public StudentProfile getStudentProfileById(@PathVariable Integer studId) {
 		loadDb.setDbData();
 		StudentProfile studProfile = new StudentProfile();
-		
+
 		StudentDetails studDetails = studentRepo.findByStudId(studId);
 		List<String> allUserNames = studentRepo.findAllUserNames(studDetails.getUserName());
-		
+
 		setBirthDetails(studProfile, studDetails.getDob());
 		studProfile.setFirstName(studDetails.getFirstName());
 		studProfile.setLastName(studDetails.getLastName());
 		studProfile.setUserName(studDetails.getUserName());
 		studProfile.setPassword(studDetails.getPassword());
 		studProfile.setAllUserNames(allUserNames);
-		return studProfile;	
+		return studProfile;
 	}
 
-	@PutMapping("/profile")
-	public void updateStudentProfile() {
+	@PutMapping("/profile/{studId}")
+	public String updateStudentProfile(@RequestBody StudentProfile newProfile, @PathVariable Integer studId) {
+		loadDb.setDbData();
+		try {
 
+			if (newProfile.getUserName() != null) {
+				studentRepo.updateUserName(newProfile.getUserName(), studId);
+			}
+
+			if (newProfile.getBirthDate() != null && newProfile.getBirthMonth() != null
+					&& newProfile.getBirthYear() != null) {
+				LocalDate dob = LocalDate.of(newProfile.getBirthYear(), newProfile.getBirthMonth(),
+						newProfile.getBirthDate());
+				studentRepo.updateDOB(dob, studId);
+			}
+			
+			return "{\"message\"" + ":" + "\"success\"}";
+		} catch (Exception e) {
+			return "{\"message\"" + ":" + "\"failed\"}";
+		}
 	}
 
 	@GetMapping("/exam/{studId}")
@@ -70,7 +93,7 @@ public class StudentController {
 		listOfExams.add(latestExam);
 		return listOfExams;
 	}
-	
+
 	@GetMapping("/exams/{studId}")
 	public List<AITRecord> getAllExams(@PathVariable Integer studId) {
 		loadDb.setDbData();
@@ -79,15 +102,15 @@ public class StudentController {
 
 	public List<AITRecord> fetchExams(Integer studId) {
 		List<AITRecord> listOfExams = new ArrayList<>();
-		listOfExams = aitRepo.findExamsByStudId(studId);	
-		
-		for(AITRecord exam : listOfExams) {
+		listOfExams = aitRepo.findExamsByStudId(studId);
+
+		for (AITRecord exam : listOfExams) {
 			exam.setTotalStudents(aitRepo.findTotalRecordsForAMocktest(exam.getMockTest()));
 			exam.setTotalStudBehind(aitRepo.findTotalStudentsBehind(exam.getTotalMarks(), exam.getMockTest()));
 		}
 		return listOfExams;
 	}
-	
+
 	private void setBirthDetails(StudentProfile studProfile, LocalDate dob) {
 		studProfile.setBirthDate(dob.getDayOfMonth());
 		studProfile.setBirthMonth(dob.getMonthValue());
--- src/main/java/sun/institute/repository/IStudentDetailsRepo.java
+++ src/main/java/sun/institute/repository/IStudentDetailsRepo.java
@@ -1,7 +1,11 @@
 package sun.institute.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +20,15 @@ public interface IStudentDetailsRepo extends CrudRepository<StudentDetails, Inte
 
 	@Query("SELECT s.userName FROM StudentDetails s WHERE s.userName !=:userName")
 	List<String> findAllUserNames(@Param("userName") String userName);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE StudentDetails s SET s.userName=:userName WHERE s.studId =:studId")
+	void updateUserName(@Param("userName") String userName, @Param("studId") Integer studId);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE StudentDetails s SET s.dob=:dob WHERE s.studId =:studId")
+	Integer updateDOB(@Param("dob") LocalDate dob, @Param("studId") Integer studId);
 
 }
